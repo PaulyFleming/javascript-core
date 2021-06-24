c1343baf4a718145788e2a940c55fe521b74297b
Paul FLeming <pfleming@heroku.com>
2021-06-24 16:36:18 +0100

    Completed module 04

---
+++ javascript-getting-started/04/demos/before/mycode/home.js
@@ -0,0 +1,10 @@
+// show the title
+let message = "Welcome";
+
+// can also declare multiple variables with a single statement
+let price = 149.99,
+    name = 'Hiking Boots',
+    discounted = false;
+
+showMessage(name + "=" + discounted);
+
